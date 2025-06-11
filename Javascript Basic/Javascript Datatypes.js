b721df486d53bdfbbf5f1fc7ed7d4ec33a799cc1
Lalithshanmugiah <lalith.shanmugiah21@gmail.com>
2025-06-11 15:22:20 +0530

    javascript basics

---
+++ Javascript Basic/Javascript Datatypes.js
@@ -0,0 +1,39 @@
+// datatypes in javascript
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+
+// ** String **
+
+// String can be declared using single quotes, double quotes, or backticks
+
+let userName = "Lalith Mohan"; // String is a sequence of characters enclosed in quotes. 
+console.log("User Name:", userName);
+console.log("User Name Type;", typeof userName); // typeof operator returns the type of the variable
+
+// ** Number **
+
+let age = 25;
+console.log("Age:", age);
+console.log("Age Type:", typeof age); // Number is a numeric value, can be an integer or a float
+
+// ** Boolean **
+
+let ispassed = true; // Boolean can be either true or false
+console.log("Is Passed:", ispassed);
+console.log("Is Passed Type:", typeof ispassed); // Boolean is a logical value
+
+// ** Undefined **
+
+let Myname;
+console.log("Name:", Myname); // Undefined variable has been declared but not assigned a value
+console.log("Name Type:", typeof Myname); // Undefined type 
+
+// ** Null **
+
+let empty = null;
+console.log("Empty:", empty); // Null is absence of any value
+console.log("Empty Type:", typeof empty); // Null type is an object,
+
