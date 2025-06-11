e6df38cc9cf1748d168da75bd5cb5185d8b1163b
Lalithshanmugiah <lalith.shanmugiah21@gmail.com>
2025-06-11 17:37:08 +0530

    Basics Of Javascript

---
+++ Javascript Basic/Concatenation and template literals.js
@@ -0,0 +1,14 @@
+// concatenation and template literals
+
+let myFirstName = "Lalith";
+let myLastName = "Mohan";
+let myAge = 25;
+let MyHobby = "Coding";
+
+console.log("My name is " + myFirstName + " " + myLastName + ". I am " + myAge + " years old and my hobby is " + MyHobby + ".");
+
+// Using template literals
+
+console.log(`My name is ${myFirstName} ${myLastName}. I am ${myAge} years old and my hobby is ${MyHobby}. `);
+
+
