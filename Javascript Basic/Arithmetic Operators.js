08b201f89f8d3f09c2f54dad9e966cab57eaf666
Lalithshanmugiah <lalith.shanmugiah21@gmail.com>
2025-06-11 17:50:55 +0530

    Basics Of Javascript

---
+++ Javascript Basic/Arithmetic Operators.js
@@ -0,0 +1,44 @@
+// Arithmetic Operators in JavaScript
+// Arithmetic operators are used to perform mathematical operations on numbers.
+// The following are the arithmetic operators in JavaScript:
+
+// 1. Addition (+)
+// 2. Subtraction (-)
+// 3. Multiplication (*)
+// 4. Division (/)
+// 5. Modulus (%)
+// 6. Exponentiation (**)
+
+// Example of Arithmetic Operators
+
+let a = 10;
+let b = 2;
+
+// Addition
+let sum = a + b;
+console.log("Addition:", sum);
+
+// Subtraction
+let sub = a - b;
+console.log("subtraction:", sub);
+
+// Multiplication
+let multi = a * b;
+console.log("Multiplication:", multi);
+
+// Division
+let div = a / b;
+console.log("Division:", div);
+
+// Modulus
+let mod = a % b;
+console.log("Modulus:", mod);
+
+// Exponentiation
+let exp = a ** 2;
+console.log("Exponentiation:", exp);
+
+// qube 
+let qube = a ** 3;
+console.log("Qube:", qube);
+
