d012425a4cf42083e828fa7b0305ae0b36d6dd1f
Lalithshanmugiah <lalith.shanmugiah21@gmail.com>
2025-06-11 14:01:32 +0530

    Javascript Basics

---
+++ Javascript Basic/Hello Program.js
@@ -0,0 +1,4 @@
+// This Is First Program in JavaScript
+
+console.log("Hello,My Name Is Lalith Mohan");
+console.log("I Am Learning JavaScript");
---
+++ Javascript Basic/Javascript Variables.js
@@ -0,0 +1,21 @@
+//Javascript Variables
+// Variables are containers for storing data values
+// In JavaScript, there are three ways to declare variables: var, let, and const
+
+// var - var is used to declare variables that can be reassigned
+
+var Myname = "lalith mohan";
+
+// let - let isused to declare variables that can be reassigned
+
+let age = 25;
+
+// const - const is used to declare variables that cannot be reassigned
+
+const DOB = 2000;
+
+// Displaying the output in the console
+
+console.log("Name:", Myname);
+console.log("Age:", age);
+console.log("Date of Birth:", DOB);
