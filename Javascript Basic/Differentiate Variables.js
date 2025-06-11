8752898a6ebe4b070964b45a1374364a0b47d057
Lalithshanmugiah <lalith.shanmugiah21@gmail.com>
2025-06-11 14:36:13 +0530

    javascript Basics

---
+++ Javascript Basic/Differentiate Variables.js
@@ -0,0 +1,24 @@
+//differentiate variables of let, var, and const
+var Stream; // var can be declared
+Stream = "computer science"; // assigning a value to var
+Stream = "Artificial Intelligence and Machine Learning"; // reassigning a value to var
+console.log("Stream:", Stream);
+
+var Stream = " computer application and information technology"; // var can be declared and assigned in one line
+Stream = "Artificial Intelligence and Machine Learning"; // reassigning a value to var
+console.log("Stream:", Stream);
+
+let Course = "B.Tech"; // let can be declared and assigned in one line
+Course = "M.Tech"; // reassigning a value to let
+console.log("Course:", Course);
+
+let course;
+course = "B.Tech"; // let can be declared and assigned in one line
+course = "M.Tech"; // reassigning a value to let
+console.log("Course:", course);
+
+
+const dateOfBirth = "21 /08/ 2000"; // const cannot be reassigned
+console.log("Date of Birth:", dateOfBirth);
+
+
